e0e95e5439d18dac733d26def77af7c23c278c14
dylanstorts <d-storts@onu.edu>
2017-01-27 18:25:50 -0500

    My first webpage

---
+++ HW/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Hello, World!</title>
+        <link rel="stylesheet" href="HW_CSS.css">
+    </head>
+    <body>
+      Hello World!
+    </body>
+</html>
