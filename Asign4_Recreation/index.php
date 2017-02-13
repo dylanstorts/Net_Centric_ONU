71866ca44a72b4930494566ec64738151c573780
dylanstorts <d-storts@onu.edu>
2017-02-13 18:39:21 -0500

    Add files via upload

---
+++ Asign4_Recreation/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Recreation: "Andersen.."</title>
+        <link rel="stylesheet" href="CSS\Recreation.css" alt="">
+    </head>
+    <body>
+        <table id="border">
+            <tr>  
+            
+                <td>
+                    <section id="left_corner"></section> 
+                    <img src="anderson_alpha.png" alt="nope" id="logo">
+                    <p id="p1">
+                        PermaView wil save you <span id="discount">15% off</span>
+                        any quote presented from any Andersen installing dealer
+                    </p>
+                    <a id="perm_link">
+                        WWW.PERMAVIEW.COM
+                    </a>
+                </td>
+           
+                <td>
+                    <p id="address">
+                        561 Schrock Rd. (west of Huntley)
+                        Columbus, OH 43229
+                    </p>
+                    <p id="phone">
+                        614-436-8226
+                    </p>
+                    <p id="hours">
+                        Hours: Monday-Thursday, 9AM-5PM;
+                    </p>
+                    <p id="hours">
+                        Friday, 9AM-Noon; Saturday, 10AM-Noon.
+                    </p>
+                    <p id="hours">
+                        Other Hours by Appointment.
+                    </p>
+                    <a id="you_link">
+                        YOUTUBE.COM/ANDERSENEXPERTS
+                    </a>
+                </td>
+           
+        </tr>
+        </table>
+        
+    </body>
+</html>
