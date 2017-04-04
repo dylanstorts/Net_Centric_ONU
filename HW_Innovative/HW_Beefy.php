bb907fa7b0cf4fc802e48e5fb35fb83a562fa83c
dylanstorts <d-storts@onu.edu>
2017-04-04 14:39:03 -0400

    Add files via upload

---
+++ HW_Innovative/HW_Beefy.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+
+<html>
+    <head>
+        <meta charset="windows-1252">
+        <title>Hello World Demo!</title>
+        <link rel="stylesheet" href="Innovative.css">
+        
+    <img src="polygonal-earth_2.jpg" id="bg" alt="">
+
+    
+    </head>
+
+    <body>
+        <p id="Title">
+        Hello World!
+        </p>
+
+
+    <table id="table" border="0" width="260" height="300">
+        <tr>
+          <th id="th"><div>My Routine</div></th>
+        </tr>
+<
+        <tr>
+            <td>
+              <a style="color:#000f2c" href="http://www.amazon.com">Amazon</a>
+            </td>
+        </tr>
+        <tr>
+            <td>
+              <a style="color:#000f2c" href="http://www.viz.com">Viz Anime</a>
+            </td>
+        </tr>
+        <tr>
+            <td>
+              <a style="color:#000f2c" href="http://www.letgo.com">LetGo</a>
+            </td>
+        </tr>
+        <tr>
+            <td>
+              <a style="color:#000f2c" href="http://northernonline.onu.edu">Moodle</a>
+            </td>
+        </tr>
+        <tr>
+            <td>
+              <a style="color:#000f2c" href="http://play.google.com">Google Play</a>
+            </td>
+        </tr>
+        
+
+    </table>
+        <p id="description_title">Needed "Routine" Site Descriptions</p>
+        <section id="anime">
+            <h1>Viz Anime</h1>
+            <p>
+                Viz is the website of a company that made my favorite cartoon of my late childhood
+                known as Inuyasha. I strongly encourage anyone with free time to follow the link and
+                search Inuyasha there. Then, you will be able to stream every single episode for free
+                and enjoy days of binging on a medieval Japanese adventure of the ages. 
+            </p>
+            <p>
+                In a gist, the story is about a teenage, Japanese girl, named Kagome, who gets pulled into a well that sends
+                her 500 years into the past! There, she faces feudal era threats such as demons, bandits and 
+                even evil spirits. Eventually she meets a half-demon named Inuyasha who initially seems power
+                hungry for a sacred jewel that grants any wish. During the feud for this jewel between Inuyasha and
+                Kagome they accidentally shattered the jewel and shards spread in all directions toward every corner of Japan 
+                during its explosion. Then they find that Kagome has spiritual powers that allow her to see and sense the jewel 
+                shards. Inuyasha then cools down and joins Kagome as he is the only one with the strength to fight the demons 
+                that possess the shards and retrieve them. As their journey continues however, they get much more than they 
+                bargained for.
+            </p>
+        </section>
+        
+    </body>
+</html>
+
