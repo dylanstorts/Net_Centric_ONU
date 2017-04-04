432eb5eb518ae20a293f9f0b8fa571067a86280b
dylanstorts <d-storts@onu.edu>
2017-04-04 14:40:10 -0400

    Personal project to explore how to manipulate images with CSS

---
+++ multimedia_proj/Multimedia_proj.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>MultiMedia Project</title>
+        <link rel="stylesheet" href="multimedia_sheet.css">
+    </head>
+    <body>
+        <div class="pdiv">
+            <p id="ausie">
+                Australia
+            </p>
+        </div>
+        <div class="pdiv" id="opener">
+            <p>
+                Australia is a place of danger and surprises. But also
+                beauty and a true sense of magic.
+            </p>
+        </div>
+        
+        <div id="div_img_rows">
+            <a id="a_roos" href="http://www.paradisepicturesonearth.com/wp-content/uploads/2016/02/australia-2.jpg">
+                <p>Three kangaroos hopping to their heart's content on the sarangeti. Click for full image.</p>
+            </a>
+
+            <a id="a_macro" href="http://4.bp.blogspot.com/-8xqXDdJWP7Q/VPM8pPgZu7I/AAAAAAAALM4/sMP5MdRyUIc/s1600/Australia%2BCover%2B02.jpg">
+                <p>Macro shot of Australia on an antique globe. Click for full resolution.</p>
+            </a>
+            
+            <a id="a_wfall" href="http://www.westernaustralia.com/SiteCollectionImages/InspirationalContent/AustraliasNorthWest/waterfall.jpg?RenditionID=1">
+                <p>Beautiful West Australian waterfall and red rock face. Click for full image.</p>
+            </a>
+        
+            <a id="a_opera" href="http://3.bp.blogspot.com/-Ka21wKoltz4/UQfQkV4_Z5I/AAAAAAAAC0o/x1vTHkb_thA/s1600/Sydney+Opera+House+at+night.jpg">
+                <p>Sydney Opera House gettin lit in beautiful colors. Click for full image.</p>
+            </a>
+        </div>
+
+        <div class="pdiv" id="secondary">
+            <p>
+                A mighty island that hosts the most majestic under water
+                world on the planet.
+            </p>
+        </div>
+        <div id="div_gbreef_imgs">
+            <a id="a_aerial" href="http://www.pre-tend.com/wp-content/uploads/2014/11/Great-Barrier-Reef.jpg">
+                <p>Stunning aerial shot of the Great Barrier Reef from a chopter. Click for full image.</p>
+            </a>
+            
+            <a id="a_couple" href="https://greatadventures.com.au/gawp/wp-content/uploads/2016/05/great-adventures-outer-barrier-reef-124145-1920.jpg">
+                <p>Couple snorkeling diving in what in the breathe taking waters of the Reef. Click for full image.</p>
+            </a>
+            
+            <a id="a_life" href="http://cdn-image.travelandleisure.com/sites/default/files/styles/1600x1000/public/1458575086/great-barrier-reef-CORAL0316.jpg?itok=V4LoeCqE">
+                <p>Close up of wildlife around coral on the Great Barrier Reef. Click for full image.</p>
+            </a>
+            
+            <a id="a_girl" href="http://www.schoolactivities.com.au/cmsAdmin/Blogg%20Images/girl-snorkeling-on-great-barrier-reef.jpg">
+                <p>Macro shot of a girl snorkeling in super shallow water of the Reef. Click for full image.</p>
+            </a>
+            
+            <a id="a_cuttle" href="http://www.greatbarrierreefs.com.au/wp-content/uploads/F909.jpg">
+                <p>The amazing cuttlefish in the warm, crystal waters off the coast of Australia. Click for full image.</p>
+            </a>
+            
+            <a id="a_uhd_life" href="https://www.cruiseexperts.com/news/wp-content/uploads/2015/04/Great-Barrier-Reef.jpg">
+                <p>Lively shot of the divergent wildlife that live in harmony on the Reef. Click for full UHD image.</p>
+            </a>
+        </div>
+    </body>
+</html>
