3decc0ae7480b6c897f8b42484dd3d9c29747e89
dylanstorts <d-storts@onu.edu>
2017-04-26 23:51:41 -0400

    Add files via upload
    A site I made early on to impress my aspiring wood, working Professor

---
+++ second_impressions_2/Second_impresssions.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Second Impressions</title>
+        <link rel="stylesheet" href="two_impressions.css">
+    </head>
+    <body id="body">
+        <p id="title">A Wood, Wood, World</p>
+        <ul id="side_list">
+            <li>
+                <a href="http://twistedsifter.com/2013/11/worlds-longest-wood-carving-zheng-chunhui/">
+                    Longest Wood Carving
+                </a>
+            </li>
+            <li>
+                <a href="http://www.theartcareerproject.com/become-woodworker/">
+                    How to Become a Woodworker
+                </a>
+            </li>
+            <li>
+                <a href="https://en.wikipedia.org/wiki/Wyoming_(schooner)">
+                Largest Wooden Ship
+                </a>
+            </li>
+            <li>
+                <a href="http://listverse.com/2011/07/30/10-magnificent-living-trees/">
+                    Top Ten Magnificent Trees
+                </a>
+            </li>
+            <li>
+                <a href="http://www.wood-database.com/wood-articles/top-ten-hardest-woods/">
+                World's Hardest Woods
+                </a>
+            </li>
+            <li>
+                <a href="http://www.wood-database.com/">
+                    The Wood Database
+                </a>
+            </li>
+        </ul>         
+        <div id="lichtenberg">
+            <p id="lichtenberg_title">Lichtenberg Figures in a Nut-Shell</p>
+            
+            <img src="lichtenberg.jpg" alt="nope" id="lichtenberg_img">
+            <img src="lichtenberg.jpg" alt="nope" id="lichtenberg_img">
+            
+            <p id="lichtenberg_body">
+                Lichtenberg figures (German Lichtenberg-Figuren), or "Lichtenberg
+                dust figures", are branching electric discharges that sometimes 
+                appear on the surface or in the interior of insulating materials. 
+                Lichtenberg figures are often associated with the progressive 
+                deterioration of high voltage components and equipment. The study 
+                of planar Lichtenberg figures along insulating surfaces and 3D 
+                electrical trees within insulating materials often provides engineers 
+                with valuable insights for improving the long-term reliability of 
+                high voltage equipment. Lichtenberg figures are now known to occur 
+                on or within solids, liquids, and gases during electrical breakdown.
+                <a href="https://en.wikipedia.org/wiki/Lichtenberg_figure">See more on Wiki</a>     
+            </p>
+
+        </div>
+            
+
+        <div id="sculptures">
+            <p id="sculptures_title">
+                Wooden Sculptures of our day
+            </p>
+            <div id="sculptures_body">
+                <p>
+                A famous French painter and sculptor, Edgar Degas, once said 
+                “Art is not what you see, but, what you make others see.” And 
+                these 20 talented and creative wood carvers will let us see the 
+                beauty embedded in the art of woodcarving through their creations. 
+                Many of these artists have also carved their mark into the 
+                international art scene.
+                </p>
+                <p>
+                Most of these sculptors have years of experience perfecting their 
+                craft. Seeing these skilled carvers transform a block of wood into 
+                an expressive artwork is really fascinating. From hand-carved to 
+                laser-cut wood sculptures made from single or multiple blocks of 
+                wood, this collection will bring enjoyment and inspiration to all 
+                art designers, not only sculptors, novices and professionals alike.
+                </p>
+            </div>
+            <a href="http://www.hongkiat.com/blog/wooden-sculptures/" id="sculpture_link">55 Amazing wooden Sculptures</a>
+        </div>
+    </body>
+</html>
